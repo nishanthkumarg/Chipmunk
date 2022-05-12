27ada0f969336881058fc8f748204701673c032b
adt <nishanthkumarg>
2022-05-12 12:06:07 -0700

    Second project

--- MyAppChip/app/src/main/java/com/example/myapplication/data/LoginDataSource.kt
+++ MyAppChip/app/src/main/java/com/example/myapplication/data/LoginDataSource.kt
@@ -1,29 +1,53 @@
-package com.example.myapplication.data;
+package com.example.myapplication.data
 
-import com.example.myapplication.data.model.LoggedInUser;
-
-import java.io.IOException;
+import com.example.myapplication.data.model.LoggedInUser
+import java.io.IOException
+import java.lang.Exception
+import java.util.*
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
-public class LoginDataSource {
-
-    public Result<LoggedInUser> login(String username, String password) {
-
-        try {
-            // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
-        } catch (Exception e) {
-            return new Result.Error(new IOException("Error logging in", e));
-        }
-    }
-
-    public void logout() {
-        // TODO: revoke authentication
-    }
+class LoginDataSource {
+	fun login(username: String?, password: String?): Result<LoggedInUser?> {
+		return try {
+			// TODO: handle loggedInUser authentication
+			val fakeUser = LoggedInUser(
+				UUID.randomUUID().toString(),
+				"Jane Doe"
+			)
+			Result.Success<Any?>(fakeUser)
+		} catch (e: Exception) {
+			Result.Error(IOException("Error logging in", e))
+		}
+	}
+	
+	fun logout() {
+		// TODO: revoke authentication
+	}
+	
+	fun CheckFoLogin() {
+		// TODO: revoke authentication
+	}
+	
+	fun CreateauthKey() {
+		// TODO: revoke authentication
+	}
+	
+	fun SendToken() {
+		// TODO: revoke authentication
+	}
+	
+	fun gettSSHTok() {
+		// TODO: revoke authentication
+	}
+	
+	fun createSSH() {
+		// TODO: revoke authentication
+	}
+	
+	// TODO: revoke authentication
+	val isLogin: Boolean
+		get() =// TODO: revoke authentication
+			false
 }
